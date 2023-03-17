c71fe1cc146c47e8818162b03ab19b39efe7b1a1
yuvaspravin <yuvaspravin@gmail.com>
2023-03-17 18:32:18 +0530

    Stages Changed

--- src/webparts/leaveManagement/LeaveManagementWebPart.ts
+++ src/webparts/leaveManagement/LeaveManagementWebPart.ts
@@ -1,31 +1,23 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { IReadonlyTheme } from "@microsoft/sp-component-base";
 
-import * as strings from 'LeaveManagementWebPartStrings';
-import LeaveManagement from './components/LeaveManagement';
-
+import * as strings from "LeaveManagementWebPartStrings";
+import LeaveManagement from "./components/LeaveManagement";
 
 export interface ILeaveManagementWebPartProps {
   description: string;
 }
 
 export default class LeaveManagementWebPart extends BaseClientSideWebPart<ILeaveManagementWebPartProps> {
-
-
-
   public render(): void {
-    const element: React.ReactElement = React.createElement(
-      LeaveManagement,
-
-
-    );
+    const element: React.ReactElement = React.createElement(LeaveManagement);
 
     ReactDom.render(element, this.domElement);
   }
@@ -34,8 +26,6 @@ export default class LeaveManagementWebPart extends BaseClientSideWebPart<ILeave
     if (!currentTheme) {
       return;
     }
-
-
   }
 
   protected onDispose(): void {
@@ -43,7 +33,7 @@ export default class LeaveManagementWebPart extends BaseClientSideWebPart<ILeave
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -51,20 +41,20 @@ export default class LeaveManagementWebPart extends BaseClientSideWebPart<ILeave
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
--- src/webparts/leaveManagement/components/LeaveManagement.tsx
+++ src/webparts/leaveManagement/components/LeaveManagement.tsx
@@ -1,7 +1,23 @@
-import * as React from 'react';
+import * as React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LeaveForm from "./LeaveForm";
+import styles from "./LeaveManagement.module.scss";
+
+import Sidebar from "./Sidebar";
 
 export default class LeaveManagement extends React.Component<{}> {
   public render(): React.ReactElement {
-    return <section>Hi Tester</section>;
+    return (
+      <section>
+        <BrowserRouter>
+          <div className={styles.sidebar}>
+            <Sidebar />
+          </div>
+          <Routes>
+            <Route path="/Apply Leave" element={<LeaveForm />} />
+          </Routes>
+        </BrowserRouter>
+      </section>
+    );
   }
 }
